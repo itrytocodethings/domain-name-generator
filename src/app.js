f274d782137bb92bc6a2ff37d6ff4181eea95a41
itrytocodethings <imwaynebailey@gmail.com>
2022-01-01 14:13:31 +0000

    completed exercise

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "last"];
+  let adjs = ["great", "big", "of"];
+  let nouns = ["jogger", "racoon", "us"];
+  let extensions = [".com", ".org", ".net", ".io", ".us"];
+
+  generateDomain(pronouns, adjs, nouns, extensions);
+
+  function generateDomain(pronouns, adjs, nouns) {
+    pronouns.forEach(pronoun => {
+      adjs.forEach(adj => {
+        nouns.forEach(noun => {
+          extensions.forEach(extension => {
+            if (`.${noun}` == extension) {
+              console.log(`${pronoun}${adj}.${noun}`);
+            } else {
+              console.log(`${pronoun}${adj}${noun}${extension}`);
+            }
+          });
+        });
+      });
+    });
+    return null;
+  }
 };
